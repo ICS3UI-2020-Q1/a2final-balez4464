2a9b61ad1b1e476e97b23b24a5c0b09210a091f7
balez4464 <balez4464@wrdsb.ca>
2020-09-16 13:13:47 +0000

    Made a program to calculate the cost of the pizza based on diameter

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program calcualtes the cost of a customers pizza based on its diameter
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +10,41 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creates a Scanner
+    Scanner input = new Scanner(System.in);
+
+    //Constants for mandatory pizza charges
+    final double LABOUR_PRICE = 0.75;
+    final double OVEN_PRICE = 0.99;
+    final double INGREDIENTS_PRICE = 0.50; 
+    final double TAX_PRICE = 0.13;
+
+    //Messgae for customers
+    System.out.println("What is the size of the pizza (in cm):");
+
+    //Variable for customers pizza diameter
+    double pizzaSize = input.nextDouble();
     
+    //Calculates subtotal for customer and tell them subtotal
+    double costNoTax = (pizzaSize * INGREDIENTS_PRICE + OVEN_PRICE + LABOUR_PRICE);
+    System.out.println("Subtotal: " + "$" + costNoTax);
+
+    //Calculates tax and tells cost
+    double taxCost = (costNoTax * TAX_PRICE);
+    System.out.println("Taxes: " + "$" + taxCost);
+    
+    //Calculates and tells costumer their total price with tax
+    double costWithTax = (costNoTax + taxCost);
+    System.out.println("Total: " + "$" + costWithTax);
+
+    //Determines which message to give user based on diameter of pizza
+    if(pizzaSize >= 1 && pizzaSize <= 20){
+      System.out.println("We are going to make you a cute little pizza!");
+    } else if(pizzaSize >= 21 && pizzaSize <= 40){
+      System.out.println("This will be delicious!");
+    } else {
+      System.out.println("Woah, big pizza! You might need a truck to get this home!");
+    }
+
   }
 }
